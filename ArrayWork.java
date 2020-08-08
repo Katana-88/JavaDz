62536665056468b0b300ba112ec930c43de98b05
Katana-88 <62358682+Katana-88@users.noreply.github.com>
2020-08-08 22:10:29 +0300

    DZ2
    Three tasks with arrays.

---
+++ ArrayWork.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+public class ArrayWork {
+
+	public static void main(String[] args) {
+	
+		//Задание один. Нечетные числа от 1 до 99
+		Integer [] arr = new Integer[50];
+		int j=0;
+		
+		for (int i=1; i<100; i++) {
+			if(i%2!=0) arr[j++]=i;
+			}
+		
+		String arrToString = "";
+		for(int x:arr){
+			arrToString += x +" ";
+			 }
+		
+		System.out.println(arrToString);
+		
+		Arrays.sort(arr, Collections.reverseOrder());
+		
+		String arrToStringReverse = "";
+		for(int x:arr){
+			arrToStringReverse += x +" ";
+			 }
+		
+		System.out.println(arrToStringReverse);		
+		System.out.print("\n");
+		
+		//Задание два. Числа Фибоначчи
+		
+		Integer [] arr2 = new Integer[20];
+		arr2[0]=1;
+		arr2[1]=1;
+		
+		for (int i=2; i<arr2.length; i++) {
+			arr2[i]=arr2[i-1]+arr2[i-2];
+			}
+		
+		for(int x:arr2){
+			System.out.print(x + " ");
+		}
+		System.out.println("\n");
+		
+		//Задание три. Двумерный массив.
+		
+		int [][] arr3 = new int [8][5];
+		Random rand=new Random();
+		for(int i=0;i<8;i++){
+			for(int k=0; k<5; k++) {
+				arr3[i][k]=rand.nextInt(90)+10;
+				System.out.print(arr3[i][k]+" ");
+			}
+			System.out.println();
+		}		
+	}
+}
