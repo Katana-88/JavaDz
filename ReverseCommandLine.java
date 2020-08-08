d7128ddd4da149dd7b3ff7ce2e87889613abb272
Katana-88 <62358682+Katana-88@users.noreply.github.com>
2020-08-08 11:15:02 +0300

    DZ1
    reverse string from args[]

---
+++ ReverseCommandLine.java
@@ -0,0 +1,11 @@
+package mainpack;
+
+public class ReverseCommandLine {
+
+	public static void main(String[] args) {
+		
+		for(int i=args.length-1; i >= 0; i--)
+			 System.out.println(args[i]);
+	}
+
+}
